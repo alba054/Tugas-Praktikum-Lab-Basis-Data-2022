ec0a01c1496f1d652f27f442d03639b611c3c9d7
isty03 <114085968+isty03@users.noreply.github.com>
2022-09-23 11:50:03 +0800

    Tugas Praktikum 1 Basis Data
    Nama : Isty Hamdayani
    NIM : H071211039

---
+++ TUGAS PRAKTIKUM 1.sql
@@ -0,0 +1,25 @@
+#CREATE DATABASE sbd1;
+-- USE sbd1;
+
+-- CREATE TABLE Office (
+-- Office_Code varchar (10) Not Null,
+-- city varchar (50) NOT NULL,
+-- phone varchar (50) NOT NULL,
+-- addresline1 varchar (50) NOT NULL, 
+-- addresline2 varchar (50),
+-- state varchar (50), 
+-- country varchar(50) NOT NULL,
+-- primary key (office_Code)
+-- );
+
+-- DESC Office;
+
+-- ALTER TABLE Office
+-- modify phone INT (20);
+
+-- ALTER TABLE Office 
+-- Drop addresline2;
+
+
+
+
